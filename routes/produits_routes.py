61fab90d22491b03b882b377a981bb4756fdc47d
cats-dell <christophe.felix@outlook.fr>
2024-12-17 11:19:43 +0100

    Creation du fichier

--- routes/produits_routes.py
+++ routes/produits_routes.py
@@ -1,53 +1,54 @@
 from flask import Blueprint, request, jsonify
 from models.database import db
-from models.user import User
+from models.produits import Produits
 
-user_routes = Blueprint("user_routes", __name__)
+produits_routes = Blueprint("produits_routes", __name__)
 
-@user_routes.route("/users", methods=["GET"])
-def get_users():
-    users = User.query.all()
-    return jsonify([user.to_dict() for user in users]), 200
+@produits_routes.route("/produits", methods=["GET"])
+def get_produits():
+    produits = Produits.query.all()
+    return jsonify([prod.to_dict() for prod in produits]), 200
 
-@user_routes.route("/users/<int:user_id>", methods=["GET"])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user:
-        return jsonify(user.to_dict()), 200
-    return jsonify({"error": "User not found"}), 404
+@produits_routes.route("/produits/<int:produit_id>", methods=["GET"])
+def get_produit(produit_id):
+    produit = Produits.query.get(produit_id)
+    if produit:
+        return jsonify(produit.to_dict()), 200
+    return jsonify({"error": "Produit not found"}), 404
 
-@user_routes.route("/users", methods=["POST"])
-def create_user():
+@produits_routes.route("/produits", methods=["POST"])
+def create_produit():
     data = request.get_json()
-    if not data.get("name") or not data.get("email"):
-        return jsonify({"error": "Name and email are required"}), 400
+    if not data.get("name") or not data.get("id")  or not data.get("description"):
+        return jsonify({"error": "Id, name and description are required"}), 400
 
-    if User.query.filter_by(email=data["email"]).first():
-        return jsonify({"error": "Email already exists"}), 400
+    if Produits.query.filter_by(id=data["id"]).first():
+        return jsonify({"error": "id already exists"}), 400
 
-    new_user = User(name=data["name"], email=data["email"])
-    db.session.add(new_user)
+    new_produit = Produits(name=data["name"], id=data["id"], description=data["description"])
+    db.session.add(new_produit)
     db.session.commit()
-    return jsonify(new_user.to_dict()), 201
+    return jsonify(new_produit.to_dict()), 201
 
-@user_routes.route("/users/<int:user_id>", methods=["PUT"])
-def update_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
+@produits_routes.route("/produits/<int:produit_id>", methods=["PUT"])
+def update_produit(produit_id):
+    produit = Produits.query.get(produit_id)
+    if not produit:
+        return jsonify({"error": "id  produit not found"}), 404
 
     data = request.get_json()
-    user.name = data.get("name", user.name)
-    user.email = data.get("email", user.email)
+    produit.name = data.get("name", produit.name)
+    produit.id = data.get("id", produit.id)
+    produit.description = data.get("description", produit.description)
     db.session.commit()
-    return jsonify(user.to_dict()), 200
+    return jsonify(produit.to_dict()), 200
 
-@user_routes.route("/users/<int:user_id>", methods=["DELETE"])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
+@produits_routes.route("/users/<int:produit_id>", methods=["DELETE"])
+def delete_user(produit_id):
+    produit = Produits.query.get(produit_id)
+    if not produit:
+        return jsonify({"error": "Produit not found"}), 404
 
-    db.session.delete(user)
+    db.session.delete(produit)
     db.session.commit()
-    return jsonify({"message": "User deleted successfully"}), 200
+    return jsonify({"message": "Produit deleted successfully"}), 200
