fa4a5982002d58724b3ff2f4b4053fd1d78473ed
cats-dell <christophe.felix@outlook.fr>
2025-02-28 10:11:56 +0100

    Test - ajout setup et teardown

--- tests/test_appels.py
+++ tests/test_appels.py
@@ -4,4 +4,16 @@ from models.database import db
 from models.appels import Appels
 
 class AppelsTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.app = create_app()
+        self.app.config["TESTING"] = True
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        self.client = self.app.test_client()
+
+        with self.app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
